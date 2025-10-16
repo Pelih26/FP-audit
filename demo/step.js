1f9f9b83d015718da926f8cc523feb07b4e9e24c
Pelih26 <kirillqa199226@gmail.com>
2025-10-16 14:35:03 +0300

    Delete demo directory

--- demo/step.js
+++
@@ -1,6 +0,0 @@
-await page.getByRole('option', { name: 'Общая' }).click();
-await page.getByRole('option', { name: 'Фотоотчёт' }).click();
-await page.getByRole('option', { name: 'Проверка наличия' }).click();
-await page.getByRole('option', { name: 'Пересчёт товара' }).click();
-await page.getByRole('option', { name: 'Сбор данных' }).click();
-await page.getByRole('option', { name: 'Подготовка к инвентаризации' }).click();
