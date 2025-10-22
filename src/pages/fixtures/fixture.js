61691801a53a240638c666743d599c04951a17c7
Kirill Pelikhovich <kirillqa199226@gmail.com>
2025-10-22 20:08:44 +0300

    revert

---
+++ src/pages/fixtures/fixture.js
@@ -0,0 +1,20 @@
+import { test as base } from '@playwright/test';
+import { MainPage, LoginUser, CreateTask } from '../index.js';
+import { App } from '../app.page.js';
+const url = 'https://audit-dev4.fix-price.ru/#/login';
+export const test = base.extend({
+  // defaultItem: ['Something nice', { option: true }],
+
+  // Нейминг
+  loginFixture: async ({ page }, use) => {
+    const app = new App(page);
+    const mainPage = new MainPage(page);
+    const loginUser = new LoginUser(page);
+    test.setTimeout(45_000);
+    await app.mainPage.open(url);
+    await app.loginUser.loginKS();
+    // Добавил Timeout что бы тест дожидался разблокировки страницы (при стабильном деве достаточно 6 сек)
+    await page.waitForTimeout(15_000);
+    await use(app);
+  },
+});
