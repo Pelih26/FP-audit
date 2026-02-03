8d1f2d705f69242bcd6596efb188e66b70c1ec88
Pelih26 <kirillqa199226@gmail.com>
2026-02-03 09:43:15 +0300

    refactor_DesignerChicklistsPage

--- support/pages/tasks/DesignerChecklistsPage.ts
+++ support/pages/tasks/DesignerChecklistsPage.ts
@@ -1,52 +1,75 @@
-import { expect, Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 import { BasePage } from '../base/BasePage.js';
+import { MainPage } from '@pages/base/MainPage';
 
 export class TemplateList extends BasePage {
-  // ====== Локаторы ======
-  // Переход по ссылкам меню
-  designerLink: import('@playwright/test').Locator;
-  templatesLink: import('@playwright/test').Locator;
-
-  // checkbox актив/деактив
-  activationFlag: import('@playwright/test').Locator;
-
-  constructor(page: Page) {
-    super(page);
+    private readonly mainPage: MainPage;
 
     // ====== Локаторы ======
     // Переход по ссылкам меню
-    this.designerLink = page.locator('a').filter({ hasText: 'Конструктор чек-листов' });
-    this.templatesLink = page.getByRole('link', { name: ' Шаблоны' });
+    private readonly designerLink = this.page.locator('a').filter({ hasText: 'Конструктор чек-листов' });
+    private readonly templatesLink = this.page.getByRole('link', { name: ' Шаблоны' });
 
     // checkbox актив/деактив
-    this.activationFlag = page.getByRole('checkbox', { name: 'Только активные' });
-  }
+    private readonly activationFlag = this.page.getByRole('checkbox', { name: 'Только активные' });
 
-  // ====== Переход на страницу задачи ======
-  async openTaskSection(): Promise<void> {
-    await this.click(this.designerLink);
-    await this.click(this.templatesLink);
-  }
+    // Инпут поиска
+    private readonly searchInput = this.page.getByRole('textbox', { name: 'Поиск по названию' });
 
-  // ====== checkbox актив/деактив ======
-  async activFlag(): Promise<void> {
-    await this.activationFlag.uncheck();
-    //await this.activationFlag.check();
-  }
+    // Создание/копирование шаблона
+    private readonly copyTemplate = this.page.getByRole('button', { name: ' Копировать шаблон' });
+    private readonly addTemplateButton = this.page.getByRole('button', { name: ' Добавить' });
+    private readonly nameTemplate = this.page.getByRole('textbox', { name: 'Название нового шаблона:' });
+    private readonly createTemplateButton = this.page.getByRole('button', { name: 'Создать' });
 
-  // ====== Проверка активных/неактивных шаблонов через API ======
-  async verifyTemplatesActivationStatus(): Promise<void> {
-    const apiResponse = await this.page.request.get('/api/admin/constructor/templates');
-    expect(apiResponse.ok()).toBeTruthy();
+    // Блок редактирование шаблона
+    private readonly redactTemplateButton = this.page.getByRole('button', { name: ' Редактировать' });
 
-    const responseData = await apiResponse.json();
+    constructor(page: Page) {
+        super(page);
+        this.mainPage = new MainPage(page);
+    }
 
-    //const hasActiveTemplates = responseData.some((template) => template.active === true);
-    const hasInactiveTemplates = responseData.some((template: any) => template.active === false);
+    // ====== Переход на страницу задачи ======
+    async openTaskSection(): Promise<void> {
+        await this.mainPage.openMenu();
+        await this.click(this.designerLink);
+        await this.click(this.templatesLink);
+    }
 
-    //expect(hasActiveTemplates, 'Проверка актив шаблонов').toBeTruthy();
-    expect(hasInactiveTemplates, 'Проверка не актив шаблонов').toBeTruthy();
+    // ====== Создание нового шаблона ======
+    async createTemplate(): Promise<void> {
+        await this.click(this.addTemplateButton);
+        await this.click(this.nameTemplate);
+        await this.fill(this.nameTemplate, 'Шаблон АвтотестQA');
+        await this.click(this.createTemplateButton);
+    }
 
-    return console.log(responseData);
-  }
-}
+    async searchTemplate(): Promise<void> {
+        await this.click(this.searchInput);
+        await this.fill(this.searchInput, 'Шаблон АвтотестQA');
+        await this.searchInput.press('Enter');
+    }
+
+    // ====== checkbox актив/деактив ======
+    async activFlag(): Promise<void> {
+        await this.activationFlag.uncheck();
+        //await this.activationFlag.check();
+    }
+
+    // ====== Проверка активных/неактивных шаблонов через API ======
+    async verifyTemplatesActivationStatus(): Promise<void> {
+        const apiResponse = await this.page.request.get('/api/admin/constructor/templates');
+        expect(apiResponse.ok()).toBeTruthy();
+
+        const responseData = await apiResponse.json();
+
+        //const hasActiveTemplates = responseData.some((template) => template.active === true);
+        const hasInactiveTemplates = responseData.some((template: any) => template.active === false);
+
+        //expect(hasActiveTemplates, 'Проверка актив шаблонов').toBeTruthy();
+        expect(hasInactiveTemplates, 'Проверка не актив шаблонов').toBeTruthy();
+
+        // return console.log(responseData);  // Возвращаем ответ
+    };
+};
