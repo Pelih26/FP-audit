ee3c977f69b21e81f8bbc09588f11439639f02a4
Kirill Pelikhovich <kirillqa199226@gmail.com>
2026-03-03 13:13:04 +0300

    fix-name-template

--- tests/createEditTemplate.spec.ts
+++ tests/createEditTemplate.spec.ts
@@ -4,7 +4,7 @@ import { App } from '@pages/App';
 let app: App;
 
 
-test.describe('templates section', () => {
+test.describe('Раздел шаблоны', () => {
     test.beforeEach(async ({ page }) => {
         // Добавил общий Timeout что бы тест длилься более 20 сек, пока прогружается главная страница после логина
         test.setTimeout(85_000);
@@ -12,19 +12,19 @@ test.describe('templates section', () => {
         await app.sideBarMenuPage.open();
     });
 
-    test('Тест - Создание нового шаблона', async ({ page }) => {
+    test('Создание нового шаблона', async ({ page }) => {
         await app.templateWizardPage.openTaskSection();
         await app.templateWizardPage.createTemplate();
         await expect(page.locator('#smallbox1')).toContainText('Шаблон успешно создан!');
     });
 
-    test('Тест - Поиск созданого шаблона', async ({ page }) => {
+    test('Поиск созданого шаблона', async ({ page }) => {
         await app.templateWizardPage.openTaskSection();
         await app.templateWizardPage.searchTemplate();
         await expect(page.getByRole('cell', { name: TestTag.TemplateName })).toBeVisible();
     });
 
-    test('Тест - Редактирования созданого шаблона', async () => {
+    test('Редактирования созданого шаблона', async () => {
         await app.templateWizardPage.openTaskSection();
     });
 });
