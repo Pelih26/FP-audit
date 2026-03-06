463c1bcea53f646f81471d4bc1d9931e43b1facc
Kirill Pelikhovich <kirillqa199226@gmail.com>
2026-03-06 10:19:09 +0300

    git-conflicts

--- support/pages/tasks/TasksAllPage.ts
+++ support/pages/tasks/TasksAllPage.ts
@@ -50,11 +50,7 @@ export class TasksAllPage extends BasePage {
     private readonly advancedSettingsTab = this.page
         .locator('div')
         .filter({ hasText: /^Дополнительные параметры$/ });
-<<<<<<< HEAD
     readonly notificationCreateTask = this.page.locator('#smallbox1');
-=======
-    readonly notificationContainer = this.page.locator('#smallbox1');
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
 
     constructor(page: Page) {
         super(page);
@@ -78,94 +74,17 @@ export class TasksAllPage extends BasePage {
 
     private async fillNameAndDescription(name: string, description: string): Promise<void> {
         await this.inputTaskName.click();
-<<<<<<< HEAD
         await this.inputTaskName.fill(name);
         await this.inputTaskDescription.click();
         await this.inputTaskDescription.fill(description);
     }
 
     private async selectCurrentDate(): Promise<void> {
-=======
-        await this.inputTaskName.fill(`${taskType} ${TestTag.QaSuffix}`);
-        await this.inputTaskDescription.click();
-        await this.inputTaskDescription.fill(`${taskType} ${TestTag.QaSuffix}`);
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
         await this.dataPicker.click();
         await this.helperData.selectCurrentDate();
     }
 
-<<<<<<< HEAD
     private async fillPerformersAndCreate(): Promise<void> {
-=======
-    async fillTaskManualRecalculation(taskType: TaskType): Promise<void> {
-        await this.inputTaskType.click();
-        const optionRecalculation = this.page.getByRole('option', {
-            name: taskType,
-        });
-        await optionRecalculation.click();
-        await this.inputTaskPriority.click();
-        await expect(this.page.locator('#input-task-name')).toHaveValue(
-            `Ручной пересчёт товаров от ${this.formattedDate}`);
-        await this.helperData.appendTextToInput('#input-task-name', ` ${TestTag.QaSuffix}`);
-        await this.inputTaskDescription.click();
-        await this.inputTaskDescription.fill(`${taskType} ${TestTag.QaSuffix}`);
-        await this.dataPicker.click();
-        await this.helperData.selectCurrentDate();
-        await this.changeButton.click();
-        await this.addMultiple.click();
-        await this.selectionProduct.click();
-        await this.selectionProduct.fill('000000000005562008');
-        await this.onList.click();
-        await this.page.waitForTimeout(600);
-        await this.applButton.click();
-        await this.performersBlok();
-    }
-
-    async fillTaskManualDataCollection(taskType: TaskType): Promise<void> {
-        await this.inputTaskType.click();
-        const optionDataCollection = this.page.getByRole('option', { name: taskType });
-        await optionDataCollection.click();
-        await this.inputTaskPriority.click();
-        await expect(this.page.locator('#input-task-name')).toHaveValue('Сбор данных для производственной отчётности');
-        await this.helperData.appendTextToInput('#input-task-name', ` ${TestTag.QaSuffix}`);
-        await this.inputTaskDescription.click();
-        await this.inputTaskDescription.fill(`${taskType} ${TestTag.QaSuffix}`);
-        await this.dataPicker.click();
-        await this.helperData.selectCurrentDate();
-        await expect(this.page.locator('#collapse_tasks-editor-general-info')).toContainText('ЛИСТОВКИ ЗАКАЗА ТАКСИ MAXIM');
-        await this.performersBlok();
-    }
-
-    async fillTaskManualInventory(taskType: TaskType): Promise<void> {
-        await this.inputTaskType.click();
-        const optionInventory = this.page.getByRole('option', { name:taskType });
-        await optionInventory.click();
-        await this.inputTaskPriority.click();
-        await expect(this.page.locator('#input-task-name')).toHaveValue(`Подготовка к инвентаризации от ${this.formattedDate}`);
-        await this.helperData.appendTextToInput('#input-task-name', ` ${TestTag.QaSuffix}`);
-        await this.inputTaskDescription.click();
-        await this.inputTaskDescription.fill(`${taskType} ${TestTag.QaSuffix}`);
-        await this.dataPicker.click();
-        await this.helperData.selectCurrentDate();
-        await this.optionalFields();
-        await this.performersBlok();
-    }
-
-    async optionalFields(): Promise<void> {
-        await this.localTime.click();
-        await this.deadLineHours.click();
-        await this.hours.click();
-        await this.deadLineMin.click();
-        //await this.advancedSettingsTab.click());
-    }
-
-    async reloadPage(taskTypeName: string): Promise<void> {
-        await this.page.reload({ waitUntil: 'networkidle' });
-        await expect(this.page.getByRole('heading', { name: taskTypeName + 'АвтотестQA' })).toBeVisible();
-    }
-
-    async performersBlok(): Promise<void> {
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
         await this.performersTab.click();
         await this.storeNumberInput.click();
         await this.storeNumberInput.fill('2815');
--- tests/createEditTemplate.spec.ts
+++ tests/createEditTemplate.spec.ts
@@ -3,12 +3,7 @@ import { TestTag } from '@pages/testData/enums/TestTag';
 import { App } from '@pages/App';
 let app: App;
 
-
-<<<<<<< HEAD
 test.describe('Раздел шаблоны', () => {
-=======
-test.describe('templates section', () => {
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
     test.beforeEach(async ({ page }) => {
         // Добавил общий Timeout что бы тест длилься более 20 сек, пока прогружается главная страница после логина
         test.setTimeout(85_000);
@@ -16,31 +11,19 @@ test.describe('templates section', () => {
         await app.sideBarMenuPage.open();
     });
 
-<<<<<<< HEAD
     test('Создание нового шаблона', async ({ page }) => {
-=======
-    test('Тест - Создание нового шаблона', async ({ page }) => {
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
         await app.templateWizardPage.openTaskSection();
         await app.templateWizardPage.createTemplate();
         await expect(page.locator('#smallbox1')).toContainText('Шаблон успешно создан!');
     });
 
-<<<<<<< HEAD
     test('Поиск созданого шаблона', async ({ page }) => {
-=======
-    test('Тест - Поиск созданого шаблона', async ({ page }) => {
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
         await app.templateWizardPage.openTaskSection();
         await app.templateWizardPage.searchTemplate();
         await expect(page.getByRole('cell', { name: TestTag.TemplateName })).toBeVisible();
     });
 
-<<<<<<< HEAD
     test('Редактирования созданого шаблона', async () => {
-=======
-    test('Тест - Редактирования созданого шаблона', async () => {
->>>>>>> e504130148c50edc5a1eb96b9f098d5d1d4e0b4f
         await app.templateWizardPage.openTaskSection();
     });
 });
