9557415c4b204fc96c076a16654d51a60c6b333e
Kirill Pelikhovich <kirillqa199226@gmail.com>
2026-03-06 10:17:43 +0300

    Merge branch 'main' of github.com:Pelih26/FP-audit

--- tests/createTack.spec.ts
+++ tests/createTack.spec.ts
@@ -13,45 +13,46 @@ test.describe('Раздел задачи', () => {
         await app.sideBarMenuPage.open();
     });
 
-    test('Создание задачи с типом "Общая"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTask(TaskType.Common);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText);
+    test('Тест - создание задачи с типом "Общая"', async ({ page }) => {
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTask(TaskType.COMMON);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут');
     });
 
-    test('Создание задачи с типом "Фотоотчёт"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTask(TaskType.PhotoReport);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText);
+    test('Тест - создание задачи с типом "Фотоотчёт"', async ({ page }) => {
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTask(TaskType.PHOTO_REPORT);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут');
     });
 
-    test('Создание задачи с типом "Проверка наличия"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTask(TaskType.Forms);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText, { timeout: 20_000 });
+    test('Тест - создание задачи с типом "Проверка наличия"', async ({ page }) => {
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTask(TaskType.FORMS);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут', { timeout: 20_000 });
     });
 
-    test('Создание задачи с типом "Пересчёт товара"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTaskManualRecalculation(TaskType.ManualRecount);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText);
+    test('Тест - создание задачи с типом "Пересчёт товара"', async ({ page }) => {
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTaskManualRecalculation(TaskType.MANUAL_RECOUNT);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут');
     });
 
-    test('Создание задачи с типом "Сбор данных"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTaskManualDataCollection(TaskType.ManualDataCollection);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText);
+    test('Тест - создание задачи с типом "Сбор данных"', async ({ page }) => {
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTaskManualDataCollection(TaskType.MANUAL_DATA_COLLECTION);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут');
     });
 
-    test('Создание задачи с типом "Подготовка к инвентаризации"', async ({ page }) => {
-        await app.tasksAllPage.openTaskSection();
-        await app.tasksAllPage.fillTaskManualInventory(TaskType.InventoryPreparation);
-        await expect(app.tasksAllPage.notificationCreateTask)
-            .toContainText(taskNotificationText);
+    test('Тест - создание задачи с типом "Подготовка к инвентаризации"', async ({ page }) => {
+        app = new App(page);
+        await app.createTask.openTaskSection();
+        await app.createTask.fillTaskManualInventory(TaskType.INVENTORY_PREPARATION);
+        await expect(page.locator('#smallbox1')).toContainText(
+            'Задача будет создана в течение 15 минут');
     });
 });
